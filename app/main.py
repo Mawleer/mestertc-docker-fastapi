7b6483b2594e23c4bfdec070e2215ca28e2c831f
Axel Mesterton <Axel.mesterton95@gmail.com>
2026-03-27 11:58:25 +0200

    Ip function

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -6,7 +6,12 @@ app = FastAPI()
 def read_root():
     return { "msg": "Hello, Mestertc!", "v": "0.2" }
 
+@app.get("/api/ip")
+def read_Ip(request: Request):
+    
+    return {"ip": request.client.host}
 
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
     return {"id": id, "q": q}
+
