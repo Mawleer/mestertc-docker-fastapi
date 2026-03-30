4c5010c229d992981d7f6c9efb0e50b43a2cad74
Axel Mesterton <Axel.mesterton95@gmail.com>
2026-03-30 11:53:28 +0300

    Add room data structure and endpoints for room retrieval

--- app/main.py
+++ app/main.py
@@ -12,6 +12,21 @@ app.add_middleware(CORSMiddleware,
     allow_headers=["*"],
 )
 
+rooms={
+        "room1": {
+            "name": "Room 1",
+            "description": "This is the single bed room."
+        },
+        "room2": {
+            "name": "Room 2",
+            "description": "This is the double bed room."
+        },
+        "room3": {
+            "name": "Room 3",
+            "description": "This is the third room."
+    }
+    }
+
 @app.get("/")
 def read_root():
     return { "msg": "Hello, Mestertc!", "v": "0.2" }
@@ -21,13 +36,12 @@ def read_Ip(request: Request):
     
     return {"ip": request.client.host}
 
-@app.get("/ip",response_class=HTMLResponse)
-def read_Ip(request: Request):
-    
-    return (f"<h2>ip: {request.client.host}</h2>")
-
 
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
     return {"id": id, "q": q}
 
+
+@app.get("/rooms")
+def read_rooms(rooms: dict = rooms):
+    return {"rooms": rooms}
